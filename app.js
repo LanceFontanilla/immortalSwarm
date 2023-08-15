f75f09584ab77e865fef434f25f21bd6a1ebf2f8
LanceFontanilla <lancefontanilla@gmail.com>
2023-08-15 17:08:28 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,120 @@
+const locations = [
+    '🏤', '🏥', '🏭', '🏢', '🏣'
+]
+
+const people = [{
+    name: 'Jimbo',
+    picture: '🤵',
+    location: '🏤'
+},
+{
+    name: 'Sammy',
+    picture: '🙆‍♀️',
+    location: '🏤'
+},
+{
+    name: 'Michael',
+    picture: '👷',
+    location: '🏤'
+},
+{
+    name: 'Robert',
+    picture: '👷',
+    location: '🏥'
+},
+{
+    name: 'Terry',
+    picture: '🤴',
+    location: '🏥',
+},
+{
+    name: 'Bill',
+    picture: '🕵️',
+    location: '🏥',
+},
+{
+    name: 'Marie',
+    picture: '👩‍🍳',
+    location: '🏭',
+},
+{
+    name: 'Mykeal',
+    picture: '💂',
+    location: '🏭',
+},
+{
+    name: 'Phil',
+    picture: '🧜‍♂️',
+    location: '🏭',
+},
+{
+    name: 'Wilson',
+    picture: '🏐',
+    location: '🏢',
+},
+{
+    name: 'Wendy',
+    picture: '👩‍⚕️',
+    location: '🏢',
+},
+{
+    name: 'Jeremy',
+    picture: '🦹',
+    location: '🏢',
+}
+]
+
+
+function postOffice() {
+    console.log("clicked postOffice 🏤")
+
+    foundPeople()
+
+}
+
+function hospital() {
+    console.log("clicked hospital 🏥")
+}
+
+function circus() {
+    console.log("clicked circus 🏭")
+}
+
+function hotel() {
+    console.log("clicked hotel 🏢")
+}
+
+function bank() {
+    console.log("click bank 🏣")
+}
+
+function drawPeople() {
+    locations.forEach((location) => {
+        console.log(location)
+        let inLocation = people.filter((person) => person.location == location)
+        console.log(inLocation)
+        let personPicture = inLocation.map((person) => person.picture + person.name)
+        console.log(personPicture)
+        document.getElementById(location).innerText = personPicture.join(',')
+
+    });
+}
+
+function foundPeople() {
+    let inLocation = people.filter((person) => {
+        if (person.picture == person.location) {
+            person.picture = '🦇'
+        }
+    }
+
+    )
+    console.log(inLocation)
+
+
+
+
+
+}
+
+
+drawPeople()
